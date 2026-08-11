2ec00c6ce722f6e3bfd0aadcd38a8c05bf7e809e
Priyam Samanta <priyamsamanta26@gmail.com>
2026-08-12 03:21:10 +0530

    Delete analytics.js

--- analytics.js
+++
@@ -1,2 +0,0 @@
-const ctx=document.getElementById('chart');
-new Chart(ctx,{type:'bar',data:{labels:['Mon','Tue','Wed','Thu','Fri','Sat','Sun'],datasets:[{label:'Study Hours',data:[1,2,1.5,3,2.5,4,2]}]},options:{responsive:true}});
